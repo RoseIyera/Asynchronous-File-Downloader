0b7b5d956933f1035dc85b6d400dacc1414680de
Rose Iyera <roseiyera@gmail.com>
2024-01-20 16:17:18 +0200

    Added the functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Asynchronous_File_Downloader
+﻿using System;
+
+namespace Asynchronous_File_Downloader
 {
     partial class Form1
     {
@@ -28,13 +30,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.materialProgressBar1 = new MaterialSkin.Controls.MaterialProgressBar();
+            this.downloadBtn = new MaterialSkin.Controls.MaterialButton();
+            this.progressLabel = new MaterialSkin.Controls.MaterialLabel();
+            this.materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            this.SuspendLayout();
+            // 
+            // materialProgressBar1
+            // 
+            this.materialProgressBar1.Depth = 0;
+            this.materialProgressBar1.Location = new System.Drawing.Point(203, 321);
+            this.materialProgressBar1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialProgressBar1.Name = "materialProgressBar1";
+            this.materialProgressBar1.Size = new System.Drawing.Size(362, 5);
+            this.materialProgressBar1.TabIndex = 0;
+            this.materialProgressBar1.Click += new System.EventHandler(this.materialProgressBar1_Click);
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.downloadBtn.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.downloadBtn.Depth = 0;
+            this.downloadBtn.HighEmphasis = true;
+            this.downloadBtn.Icon = null;
+            this.downloadBtn.Location = new System.Drawing.Point(311, 227);
+            this.downloadBtn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.downloadBtn.MouseState = MaterialSkin.MouseState.HOVER;
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.NoAccentTextColor = System.Drawing.Color.Empty;
+            this.downloadBtn.Size = new System.Drawing.Size(135, 36);
+            this.downloadBtn.TabIndex = 1;
+            this.downloadBtn.Text = "Download File";
+            this.downloadBtn.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.downloadBtn.UseAccentColor = false;
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.materialButton1_Click);
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Depth = 0;
+            this.progressLabel.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.progressLabel.Location = new System.Drawing.Point(346, 286);
+            this.progressLabel.MouseState = MaterialSkin.MouseState.HOVER;
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(63, 19);
+            this.progressLabel.TabIndex = 2;
+            this.progressLabel.Text = "Progress";
+            this.progressLabel.Click += new System.EventHandler(this.materialLabel1_Click);
+            // 
+            // materialTextBox1
+            // 
+            this.materialTextBox1.AnimateReadOnly = false;
+            this.materialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialTextBox1.Depth = 0;
+            this.materialTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBox1.LeadingIcon = null;
+            this.materialTextBox1.Location = new System.Drawing.Point(227, 153);
+            this.materialTextBox1.MaxLength = 50;
+            this.materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBox1.Multiline = false;
+            this.materialTextBox1.Name = "materialTextBox1";
+            this.materialTextBox1.Size = new System.Drawing.Size(314, 50);
+            this.materialTextBox1.TabIndex = 3;
+            this.materialTextBox1.Text = "";
+            this.materialTextBox1.TrailingIcon = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.materialTextBox1);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.materialProgressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
+
         #endregion
+
+        private MaterialSkin.Controls.MaterialProgressBar materialProgressBar1;
+        private MaterialSkin.Controls.MaterialButton downloadBtn;
+        private MaterialSkin.Controls.MaterialLabel progressLabel;
+        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace Asynchronous_File_Downloader
 {
-    public partial class Form1 : Form
+    public class ProgressChangedEventArgs : EventArgs
     {
+        public long DownloadedBytes { get; }
+        public long TotalBytes { get; }
+
+        public ProgressChangedEventArgs(long downloadedBytes, long totalBytes)
+        {
+            DownloadedBytes = downloadedBytes;
+            TotalBytes = totalBytes;
+        }
+    }
+
+    public class AsyncFileDownloader
+    {
+        public delegate void ProgressChangedEventHandler(object sender, ProgressChangedEventArgs e);
+        public event ProgressChangedEventHandler ProgressChanged;
+
+        public async Task DownloadFileAsync(string url, string destinationPath)
+        {
+            using (var httpClient = new HttpClient())
+            using (var fileStream = File.OpenWrite(destinationPath))
+            {
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var totalBytes = response.Content.Headers.ContentLength ?? -1L;
+                long downloadedBytes = 0;
+
+                using (var contentStream = await response.Content.ReadAsStreamAsync())
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                        downloadedBytes += bytesRead;
+
+                        // Raise progress notification
+                        OnProgressChanged(new ProgressChangedEventArgs(downloadedBytes, totalBytes));
+                    }
+                }
+            }
+        }
+
+        protected virtual void OnProgressChanged(ProgressChangedEventArgs e)
+        {
+            ProgressChanged?.Invoke(this, e);
+        }
+    }
+
+    public partial class Form1 : MaterialForm
+    {
+        private AsyncFileDownloader downloader;
+
         public Form1()
         {
             InitializeComponent();
+
+            // Initialize the AsyncFileDownloader
+            downloader = new AsyncFileDownloader();
+            downloader.ProgressChanged += Downloader_ProgressChanged;
+        }
+
+        private void Downloader_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            double progress = (double)e.DownloadedBytes / e.TotalBytes * 100;
+            progressLabel.Text = $"Progress: {progress:F2}%";
+            materialProgressBar1.Value = (int)progress;
+        }
+
+        private async void materialButton1_Click(object sender, EventArgs e)
+        {
+            // Specify the URL and destination path
+            string url = "https://example.com/large-file.zip";
+            string destinationPath = "C:\\downloads\\large-file.zip";
+
+            // Trigger file download
+            await downloader.DownloadFileAsync(url, destinationPath);
+        }
+
+        private void materialProgressBar1_Click(object sender, EventArgs e)
+        {
+            // Handle click event if needed
+        }
+
+        private void materialLabel1_Click(object sender, EventArgs e)
+        {
+            // Handle click event if needed
         }
     }
 }
